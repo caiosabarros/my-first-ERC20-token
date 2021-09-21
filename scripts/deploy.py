bd91ad260b303c750f17d32132c65fd65be5f5f6
Caio Sa <caiohenriquesa1@gmail.com>
2021-09-21 09:52:25 -0300

    interacting with interface without making a state change

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -8,5 +8,12 @@ def deploy_my_token(initial_supply=INITIAL_SUPPLY):
   my_token = MyToken.deploy(initial_supply,{"from":account})
   print(f"Contract deployed to {my_token.address}")
 
+def total_ammount():
+  account = get_account()
+  token = MyToken[-1]
+  total = token.totalSupply() #Since it is not a state change, I don't need to include {"from":account}
+  print(f"The total supply is {total}") 
+
 def main():
   deploy_my_token()
+  total_ammount()
